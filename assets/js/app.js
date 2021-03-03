892cc2e379bfb136db10c0fc7b231c848f104b59
YBogdan RADOI <radoi.office@gmail.com>
2021-03-03 13:21:54 +0100

    Correction bug flashs

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,8 +5,6 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
-// any CSS you import will output into a single css file (app.scss in this case)
+// Importer app.scss
 import '../css/app.scss';
 
-// start the Stimulus application
-// import './bootstrap';
